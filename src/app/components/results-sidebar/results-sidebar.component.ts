504f8f41f1ba18ccad9727c724ac65125a198e75
esousacosta <victoremanoel23@gmail.com>
2023-12-15 06:26:01 +0100

    Add checkboxes with price filters - ogng

--- src/app/components/results-sidebar/results-sidebar.component.ts
+++ src/app/components/results-sidebar/results-sidebar.component.ts
@@ -1,4 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
+
+declare class CheckBox {
+  checked: boolean;
+  min: number;
+  max: number;
+}
 
 @Component({
   selector: 'olv-results-sidebar',
@@ -10,6 +17,50 @@ export class ResultsSidebarComponent {
   private _sliderMinValue: number = 0;
   @Input()
   private _sliderMaxValue: number = 500;
+  private _minSelectedPrice: number = 0;
+  private _maxSelectedPrice: number = 500;
+  private _priceCheckBoxes: Map<number, CheckBox> = new Map<number, CheckBox>([
+    [
+      0,
+      {
+        checked: false,
+        min: 0,
+        max: 50,
+      },
+    ],
+    [
+      1,
+      {
+        checked: false,
+        min: 50,
+        max: 100,
+      },
+    ],
+    [
+      2,
+      {
+        checked: false,
+        min: 100,
+        max: 150,
+      },
+    ],
+    [
+      3,
+      {
+        checked: false,
+        min: 150,
+        max: 200,
+      },
+    ],
+    [
+      4,
+      {
+        checked: false,
+        min: 200,
+        max: 500,
+      },
+    ],
+  ]);
 
   get sliderMinValue(): number {
     return this._sliderMinValue;
@@ -18,9 +69,6 @@ export class ResultsSidebarComponent {
   get sliderMaxValue(): number {
     return this._sliderMaxValue;
   }
-
-  private _minSelectedPrice: number = 0;
-  private _maxSelectedPrice: number = 500;
 
   get minSelectedPrice(): number {
     return this._minSelectedPrice;
@@ -34,5 +82,17 @@ export class ResultsSidebarComponent {
   }
   set maxSelectedPrice(iMaxValue: number) {
     this._maxSelectedPrice = iMaxValue;
+  }
+
+  filterPriceByCheckbox($event: MatCheckboxChange, iId: number) {
+    if ($event.checked) {
+      console.log($event);
+      // how to modify the checked attribute
+      // of the value in the map?
+      this._priceCheckBoxes.get(iId);
+    } else {
+      console.log('Unchecked');
+    }
+    console.log(this._priceCheckBoxes);
   }
 }
